f2d6afda9ebbf5476e3005e34770108162f430e8
ItzNotDD3 <dwd6@sunyorange.edu>
2022-11-17 12:26:58 -0500

    edit

--- analyze/scripts/script.js
+++ analyze/scripts/script.js
@@ -1,12 +1,31 @@
 /*
-    Student Name:
+    Student Name: Derrick Davis
     File Name: script.js
-    Date: 
+    Date: 11/17/2022
 */
 
+//global variables
+var figElement = document.getElementById("placeholder");
+var imgSource = document.getElementById("image");
+var figCap = doucment.query.Selector("figcaption");
 //Function to display the first picture
-function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+function pic1() {
+    imgSource.src = "images/trunk-bay.jpg";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
+}
+//Function to display the second picture
+function pic2() {
+    imgSource.src = "images/sanjuan.jpg";
+    imgSource.alt = "Elevated view of San Juan coast";
+    figElement.style.display = "block";
+    figCap.textContent = "Coast of San Juan";
+}
+//Function to display the third picture
+function pic3() {
+    imgSource.src = "images/curacao.jpg";
+    imgSource.alt = "The blue water of Curacao";
+    figElement.style.display = "block";
+    figCap.textContent = "Curacao";
+}
