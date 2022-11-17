fab95886da815414d5de71c12ac73a648fca3b19
ItzNotDD3 <dwd6@sunyorange.edu>
2022-11-17 17:44:00 -0500

    edit

--- analyze/scripts/script.js
+++ analyze/scripts/script.js
@@ -1,31 +1,33 @@
 /*
-    Student Name: Derrick Davis
+    Student Name: Christian Morgan
     File Name: script.js
-    Date: 11/17/2022
+    Date: 11/17/22
 */
 
-//global variables
+
 var figElement = document.getElementById("placeholder");
 var imgSource = document.getElementById("image");
-var figCap = doucment.query.Selector("figcaption");
+var figCap = document.querySelector("figcaption");
+
 //Function to display the first picture
-function pic1() {
+function pic1 {
     imgSource.src = "images/trunk-bay.jpg";
     imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
     figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
 }
-//Function to display the second picture
-function pic2() {
+//Function to display the Second picture
+function pic2 {
     imgSource.src = "images/sanjuan.jpg";
     imgSource.alt = "Elevated view of San Juan coast";
     figElement.style.display = "block";
     figCap.textContent = "Coast of San Juan";
-}
+  }
+
 //Function to display the third picture
-function pic3() {
+function pic2 {
     imgSource.src = "images/curacao.jpg";
-    imgSource.alt = "The blue water of Curacao";
+    imgSource.alt = "The blue waters of Curacao";
     figElement.style.display = "block";
     figCap.textContent = "Curacao";
-}
+  }
--- extend/scripts/script.js
+++ extend/scripts/script.js
@@ -1,5 +1,15 @@
 /*
-    Student Name:
+    Student Name: Christian Morgan
     File Name: script.js
-    Date: 
+    Date:11/15/22
 */
+
+function myFunction() {
+  alert("I am an alert box!");
+  var x = document.getElementById("first");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
